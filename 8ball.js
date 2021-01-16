08c292f622bb95b740b008c7f9fe23a12815d4c5
RaiderxD404 <74471862+RaiderxD404@users.noreply.github.com>
2021-01-16 14:30:54 -0500

    Update 8ball.js

--- 8ball.js
+++ 8ball.js
@@ -2,7 +2,7 @@
 
 if(command === "8ball"){ // el comando xD
   if(!message.content.startsWith(prefix)) return; //aqui hacemos que el bot no responda con otros prefixes (Es algo raro la verdad)
-  var respuesta = ["Si", "No", "Tal vez", "Obvio", "Yo digo que si", "Yo digo que no", "Probablemente"] (las respuestas)
+  var respuesta = ["Si", "No", "Tal vez", "Obvio", "Yo digo que si", "Yo digo que no", "Probablemente"] //(las respuestas)
   var random = respuesta[Math.floor(Math.random() * respuesta.length)] //hacemos un math random y definimos random para usarlo luego
   if (!args.length) return message.channel.send(`Escribe tu pregunta.`);  //hacemos un return por si no pone un argumento
 const embed = new MessageEmbed() //hacemos el embed (Si tienen definido discord como "Discord" o "discord" tienen que poner Discord.MessageEmbed o discord.MessageEmbed
